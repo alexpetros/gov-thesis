e563763b4d418bf1d78b3061d679d6084c816f84
Alexander Petros <apetros15@gmail.com>
2019-04-02 20:25:20 -0400

    wolforth version 3-30

--- 3-humint/humint-scripts.R
+++ 3-humint/humint-scripts.R
@@ -3,11 +3,15 @@ library(stringr)
 library(xtable)
 library(ggthemes)
 library(tikzDevice)
+library(ggrepel)
 
+SAVE_OUTPUT <- FALSE
 
-spies <- read_csv('./american-spies.csv')
+# mickoulus alleged apies ---------------------------------------------------------
 
-clean_spies <- spies %>% 
+alleged_spies <- read_csv('./american-spies.csv')
+
+clean_spies <- alleged_spies %>% 
   mutate(year = str_extract(Date1, "([0-9]{4})")) %>% 
   rename(spy = Spy) %>% 
   select(spy, year) %>% 
@@ -27,17 +31,36 @@ decade_spies <- ggplot(data = spies_by_year) +
   labs(y='Alleged American Spies', x='Decade') +
   theme_few()
 
+# output a CSV
+if (SAVE_OUTPUT) write_csv(clean_spies, './american-spies-clean.csv')  
 
-
-# output ------------------------------------------------------------------
-
-# write_csv(clean_spies, './american-spies-clean.csv')
-
+# output a LaTeX table
 latex <- clean_spies %>% 
   filter(year > 1945)
-# print(xtable(latex), include.rownames=FALSE)
+if (SAVE_OUTPUT) {
+  tikz('decade_spies.tex',width=5, height=4)
+  decade_spies
+  dev.off()
+}
 
-tikz('decade_spies.tex',width=5, height=4)
-decade_spies
-dev.off()
 
+# perserec active spies ----------------------------------------------------------
+
+active_spies <- read_csv('./perserec-active-spies.csv')
+
+emph_years = c(1953, 1960, 1973, 1985, 1995)
+perserec_spies <- active_spies %>% 
+  mutate(emph_points = ifelse(year %in% emph_years, num_spies, NA))
+
+perserec_spies_chart <- ggplot(data = perserec_spies) + 
+  geom_line(mapping = aes(x=year, y=num_spies), size = 1.5, stat = 'identity', color = 'darkblue') +
+  geom_point(mapping = aes(x=year, y=emph_points), color = 'red') +
+  geom_label_repel(mapping = aes(x=year, y=num_spies, label=emph_points)) +
+  labs(x = 'Year', y = 'Active American Spies') +
+  theme_few()
+
+if (SAVE_OUTPUT) {
+  tikz('perserec_spies.tex',width=6.5, height=4)
+  perserec_spies_chart
+  dev.off()
+}
